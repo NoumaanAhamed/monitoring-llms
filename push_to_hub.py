2da36e4138ad2bb7d8ddf85f48bf2dca1e6ca9bc
NoumaanAhamed <noumaanahaamed2@gmail.com>
2024-07-07 15:45:09 +0000

    push

---
+++ push_to_hub.py
@@ -0,0 +1,35 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+import torch
+
+model_name = "microsoft/Phi-3-mini-128k-instruct"
+
+from huggingface_hub import login
+
+# login(token=)
+
+# Configure 4-bit quantization
+quantization_config = BitsAndBytesConfig(
+    load_in_4bit=True)
+
+# Load the model with 4-bit quantization
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    quantization_config=quantization_config,
+    device_map="auto",
+    trust_remote_code=True
+)
+
+# Load the tokenizer
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+
+
+hf_username = "Noumaan"
+repo_name = "phi3-mini-128k-instruct-4bit-quantized"
+
+# Create the full repository name
+repo_id = f"{hf_username}/{repo_name}"
+
+# Push the model and tokenizer to the Hub
+model.push_to_hub(repo_id)
+tokenizer.push_to_hub(repo_id)
